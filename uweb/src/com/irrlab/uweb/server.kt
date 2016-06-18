84c44634dc5faf08e24cd287a5da960182294c34
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2016-06-18 13:46:29 -0300

    uweb

---
+++ uweb/src/com/irrlab/uweb/server.kt
@@ -0,0 +1,15 @@
+package com.irrlab.uweb
+
+import io.undertow.Undertow
+import io.undertow.util.Headers
+
+fun main(args : Array<String>) {
+
+    val server = Undertow.builder().addHttpListener(8080, "localhost").setHandler { exchange ->
+        exchange.responseHeaders.put(Headers.CONTENT_TYPE, "text/plain")
+        exchange.responseSender.send("Hello World")
+    }.build()
+
+    server.start()
+}
+
